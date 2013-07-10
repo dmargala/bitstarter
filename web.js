749273e6d05b0944c7866eb8dc84836d9a3a5dc3
Daniel Margala <djmargala@gmail.com>
2013-07-10 23:29:04 +0000

    Fix bug in web.js: call correct function fs.readFileSync

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  mybuffer = fs.readFile('index.html');
+  mybuffer = fs.readFileSync('index.html');
   response.send(mybuffer.toString());
 });
 
